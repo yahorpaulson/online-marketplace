1a991c47c94143e2503cc24c49d69a016b4f41bf
yahorpaulson <y.paulson@yahoo.com>
2024-12-20 13:57:57 +0100

    routes for main and retail components are set up

--- online-marketplace/src/app/app.routes.ts
+++ online-marketplace/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RetailComponent } from './retail/retail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'retail', component: RetailComponent },
+    { path: '', component: HomeComponent }
+];
---
+++ online-marketplace/src/app/retail/retail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-retail',
+  imports: [],
+  templateUrl: './retail.component.html',
+  styleUrl: './retail.component.css'
+})
+export class RetailComponent {
+
+}
