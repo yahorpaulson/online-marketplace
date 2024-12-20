7e85bb23f78c353b4684ad44e68da9d2e1a0358d
yahorpaulson <y.paulson@yahoo.com>
2024-12-20 15:03:32 +0100

    created product-list template showing available products

--- online-marketplace/src/app/app.routes.ts
+++ online-marketplace/src/app/app.routes.ts
@@ -2,8 +2,9 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RetailComponent } from './retail/retail.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 export const routes: Routes = [
-    { path: 'retail', component: RetailComponent },
+    { path: 'retail', component: RetailComponent, children: [{ path: '', component: ProductListComponent }] },
     { path: '', component: HomeComponent }
 ];
---
+++ online-marketplace/src/app/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [CommonModule],
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = [
+    { id: 1, name: 'Product 1', price: 100 },
+    { id: 2, name: 'Product 2', price: 200 },
+    { id: 3, name: 'Product 3', price: 300 },
+  ];
+}
