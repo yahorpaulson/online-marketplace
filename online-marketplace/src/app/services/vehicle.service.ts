7a0aafa034723a081cd9b5ecd0aa2430aa013a07
AhmedIBRAHIM01 <103690053+AhmedIBRAHIM01@users.noreply.github.com>
2025-01-24 16:20:51 +0100

    feat: add Vehicle.Service to comuicate to Backend

---
+++ online-marketplace/src/app/services/vehicle.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { VehicleFactoryService } from './vehicle-factory.service';
+import { Vehicle } from '../models/Vehicle';
+import { VehicleData } from '../models/VehicleData';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VehicleService {
+  private apiUrl = 'http://localhost:4000/api/vehicles'; // API-Endpunkt
+
+  constructor(
+    private http: HttpClient,
+    private vehicleFactory: VehicleFactoryService
+  ) {}
+
+  /**
+   * Get all vehicles from the API and map them to specific vehicle instances.
+   * @returns Observable of Vehicle array
+   */
+  getVehicles(): Observable<Vehicle[]> {
+    return this.http.get<VehicleData[]>(this.apiUrl).pipe(
+      map((vehicleDataArray) =>
+        vehicleDataArray.map((data) => this.vehicleFactory.createVehicleInstance(data))
+      )
+    );
+  }
+
+  /**
+   * Get a single vehicle by ID from the API.
+   * @param id - The ID of the vehicle
+   * @returns Observable of a specific Vehicle instance
+   */
+  getVehicleById(id: number): Observable<Vehicle> {
+    return this.http.get<VehicleData>(`${this.apiUrl}/${id}`).pipe(
+      map((data) => this.vehicleFactory.createVehicleInstance(data))
+    );
+  }
+
+  /**
+   * Add a new vehicle to the system.
+   * @param vehicleData - Data for the new vehicle
+   * @returns Observable of the created vehicle
+   */
+  addVehicle(vehicleData: VehicleData): Observable<Vehicle> {
+    return this.http.post<VehicleData>(this.apiUrl, vehicleData).pipe(
+      map((data) => this.vehicleFactory.createVehicleInstance(data))
+    );
+  }
+
+  /**
+   * Update an existing vehicle.
+   * @param id - The ID of the vehicle to update
+   * @param vehicleData - Updated data for the vehicle
+   * @returns Observable of the updated vehicle
+   */
+  updateVehicle(id: number, vehicleData: VehicleData): Observable<Vehicle> {
+    return this.http.put<VehicleData>(`${this.apiUrl}/${id}`, vehicleData).pipe(
+      map((data) => this.vehicleFactory.createVehicleInstance(data))
+    );
+  }
+
+  /**
+   * Delete a vehicle by ID.
+   * @param id - The ID of the vehicle to delete
+   * @returns Observable of void
+   */
+  deleteVehicle(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
