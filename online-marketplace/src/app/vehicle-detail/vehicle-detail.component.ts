658d1881ca325334090ba76178a10376c1ac2205
AhmedIBRAHIM01 <103690053+AhmedIBRAHIM01@users.noreply.github.com>
2025-01-27 21:50:27 +0100

    feat: complete vehicle-detail component

--- online-marketplace/src/app/vehicle-detail/vehicle-detail.component.ts
+++ online-marketplace/src/app/vehicle-detail/vehicle-detail.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { VehicleService } from '../services/vehicle.service';
+import { Vehicle } from '../models/Vehicle';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vehicle-detail',
-  imports: [],
   templateUrl: './vehicle-detail.component.html',
-  styleUrl: './vehicle-detail.component.css'
+  styleUrls: ['./vehicle-detail.component.css'],
+  imports:[CommonModule]
 })
-export class VehicleDetailComponent {
+export class VehicleDetailComponent implements OnInit {
+  vehicle: Vehicle | null = null;
+  loading: boolean = true;
+  errorMessage: string | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private vehicleService: VehicleService,
+    private router:Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.loadVehicleDetail();
+  }
+
+  private loadVehicleDetail(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.vehicleService.getVehicleById(+id).subscribe({
+        next: (data) => {
+          this.vehicle = data;
+          this.loading = false;
+        },
+        error: (err) => {
+          this.errorMessage = 'Error loading vehicle details. Please try again later.';
+          console.error('Error:', err);
+          this.loading = false;
+        }
+      });
+    } else {
+      this.errorMessage = 'Invalid vehicle ID.';
+      this.loading = false;
+    }
+  }
+  goBack(): void {
+    this.router.navigate(['/vehicles']); // Adjust the route as needed
+  }
 }
